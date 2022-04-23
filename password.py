cbc65ec50d5fc5b183510dda053e0b84e7d6c18a
beth nduta <bethnduta05@gmail.com>
2022-04-23 14:04:00 +0300

    modified .py

--- password.py
+++ password.py
@@ -31,5 +31,46 @@ class User:
         '''
         method that returns info from the userslist
         '''
-        return cls.UserList      
-
+        return cls.UserList  
+    @classmethod
+    def find_by_number(cls,number):
+        '''
+        method that takes in a username and returns a user that matches that number
+        '''
+        for user in cls.UserList:
+            if user.password == number:
+                return user
+        @classmethod
+        def user_exist(cls,number):
+            for user in cls.userslist:
+                if user.username == number:
+                    return True
+                    return False
+            class Credentials:
+                '''
+                class that generates new instances of credentials
+                '''
+                accounts =[]
+                def __init__(self, accountusername, accountname,accountpassword):
+                        ''' 
+                        __init__ method that helps us define prpoerties for our objectself
+                        '''
+                        self.accountusername = accountusername
+                        self.accountname = accountname
+                        self.accountpassword = accountpassword
+                def save_account(self):
+                        '''
+                        save_account method saves user info into accounts
+                        '''                           
+                        Credentials.accounts.append(self)
+                        '''
+                        save account method saves user info into accounts
+                        '''
+                    def delete_account(self):
+                        '''
+                        delete_account method deletes a saved credential from accounts
+                        '''
+                        Credentials.accounts.remove(self)
+                        @classmethod
+                        def display_accounts(cls):
+                            
