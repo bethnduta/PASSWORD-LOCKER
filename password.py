e5e37dbe089e2c2d4e305610fb5dfeae71d25861
beth nduta <bethnduta05@gmail.com>
2022-04-23 13:33:01 +0300

    made changes

--- password.py
+++ password.py
@@ -1,12 +1,25 @@
-from unicodedata import name
+from collections import UserList
+import random
+import string
 
-
-class user:
-
-    user_list = []
-    def__init__(self.user_name.password);
-    self.user_name = user_name
-    self.password = password
-
+class User:
+    '''
+    class that generates new instances of users
+    '''
+    UserList = []
+    def __init__(self,firstname,lastname,username,password):
+        '''
+        __init__method that helps us define properties for our objectself
+        '''
+        self.firstname=firstname
+        self.lastname=lastname
+        self.username=username
+        self.password=password
     def save_user(self):
-        user.user_list.append(self)
+        '''
+        save_user method saves user info into user userslist
+        '''
+        User.usersList.append(self)
+
+    def delete    
+
