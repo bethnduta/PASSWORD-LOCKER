70b063e5ad7da005d471016cac0249e9df6e0ba3
beth nduta <bethnduta05@gmail.com>
2022-04-23 13:02:27 +0300

    modified password test

--- password_test.py
+++ password_test.py
@@ -4,3 +4,20 @@ from password import user
 class Testuser(unittest.TestCase):
     def setUp(self):
         def test_init(self):
+            '''
+            test_init test case to test if the object is initialized properly
+            '''
+            self.assertEqual(self.new_user.user_name, "Beth")
+
+            self.assertEqual(self.new_user.password, "0780")
+        def test_save_user(self):
+            '''
+            test_save_user test case to test if the user object is saved into the user list
+            '''
+            self.new_user.save_user() #saving the new contact
+            self.assertEqual(len(user.user_list),1)
+        def test_delete_user(self):
+            ''' 
+            test_delete_contact to test if we can remove our contact from our contact list
+            ''' 
+              
